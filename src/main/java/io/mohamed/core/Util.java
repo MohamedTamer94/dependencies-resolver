a2ae6155cbf0d3955bae3c4a18dbb5bf6db461e4
MohamedTamer94 <mohamed_tamer_ahmed@yahoo.com>
2021-09-05 20:46:42 +0200

    fix: version couldn't be retrieved properly
    Signed-off-by: MohamedTamer94 <mohamed_tamer_ahmed@yahoo.com>

--- src/main/java/io/mohamed/core/Util.java
+++ src/main/java/io/mohamed/core/Util.java
@@ -1,13 +1,10 @@
 package io.mohamed.core;
 
+import io.mohamed.core.version.Version;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringReader;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.Properties;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 
@@ -48,19 +45,7 @@ public class Util {
   }
 
   public static String getVersion() {
-    try {
-      ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-      InputStream inputStream = classloader.getResourceAsStream("/version.properties");
-      if (inputStream == null) {
-        return "This Build doesn't have a version name provided.";
-      }
-      Properties properties = new Properties();
-      properties.load(new StringReader(IOUtils.toString(inputStream, StandardCharsets.UTF_8)));
-      String version = properties.getProperty("version");
-      return "Dependencies Resolver - v" + version;
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    return "This Build doesn't have a version name provided.";
+    // the Version class is generated in compile time in build.gradle
+    return "Dependency Resolver - Version " + Version.VERSION;
   }
 }
